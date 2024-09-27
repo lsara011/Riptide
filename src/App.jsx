1ba94a3ac03baf9b8056938606e5c96b1e524439
lsara011 <saravia2019@hotmail.com>
2024-09-27 00:34:40 -0400

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,21 @@ function Header(){
             <h1>RipTide</h1>
           </div>
           <ul>
-            <a href="#"><li>About</li></a>
-            <a href="#"><li>Services</li></a>
-            <a href="#"><li>Contact</li></a>
-            <a href="#"><li>Careers</li></a>
+            <li><a href="#">Main Screen</a></li>
+            <li><a href="#" onClick={() => setCurrentSection('behindProject')}>Behind The Project</a></li>
+            <li><a href="https://luisfsaravia.com" target="_blank" rel="noopener noreferrer">About The Dev</a></li>
+            <li><a href="#" onClick={() => setCurrentSection('toolsUsed')}>Tools Used</a></li>
           </ul>  
         </nav>  
       </header>      
+    </>
+  )
+}
+
+function behindProject(){
+  return(
+    <>
+      <h1>Testing</h1>
     </>
   )
 }
