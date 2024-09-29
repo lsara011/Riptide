90ef043ebc8776a20a6215c936953448f8c88092
Luis Saravia <129925608+lsara011@users.noreply.github.com>
2024-09-29 13:57:50 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <Header />
+      <Header/>
+      <section>
+        <MainSection />
+      </section>
     </>
   )
 }
@@ -13,30 +19,33 @@ function Header(){
   return(
     <>
       <header>
+        <div className="logoHeader">
+          <h1 className='logoText'>HandsOUt4Help</h1>
+        </div>
         <nav>
-          <div className="logo">
-            <h1>RipTide</h1>
+          <div className="nav-links">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Resources</a></li>
+              <li><a href="#">Self-Care Tips</a></li>
+              <li><a href="https://luisfsaravia.com" target='
+              _blank'>About the Developer</a></li>
+            </ul>
           </div>
-          <ul>
-            <li><a href="#">Main Screen</a></li>
-            <li><a href="#" onClick={() => setCurrentSection('behindProject')}>Behind The Project</a></li>
-            <li><a href="https://luisfsaravia.com" target="_blank" rel="noopener noreferrer">About The Dev</a></li>
-            <li><a href="#" onClick={() => setCurrentSection('toolsUsed')}>Tools Used</a></li>
-          </ul>  
-        </nav>  
-      </header>      
+        </nav>
+      </header>
     </>
   )
 }
 
-function behindProject(){
+function MainSection(){
   return(
     <>
-      <h1>Testing</h1>
+      <div className="section-title">
+        <h1>The Purpose</h1>
+      </div>
     </>
   )
 }
-
-
 
 export default App
