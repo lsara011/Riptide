d81d122665973b7e32beb7f558e4663b68709955
lsara011 <saravia2019@hotmail.com>
2024-09-30 00:23:53 -0400

    mobile responsive and header fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,16 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Section from './section'
+import { Header } from './Header'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Section/>
     </>
   )
 }
-
-function Header(){
-  return(
-    <>
-      <header>
-        <nav>
-          <div className="logo">
-            <h1>RipTide</h1>
-          </div>
-          <ul>
-            <li><a href="#">Main Screen</a></li>
-            <li><a href="#" onClick={() => setCurrentSection('behindProject')}>Behind The Project</a></li>
-            <li><a href="https://luisfsaravia.com" target="_blank" rel="noopener noreferrer">About The Dev</a></li>
-            <li><a href="#" onClick={() => setCurrentSection('toolsUsed')}>Tools Used</a></li>
-          </ul>  
-        </nav>  
-      </header>      
-    </>
-  )
-}
-
-function behindProject(){
-  return(
-    <>
-      <h1>Testing</h1>
-    </>
-  )
-}
-
-
 
 export default App
